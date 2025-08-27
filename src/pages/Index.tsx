6a3e87bf5ba2ef421009eac3c54c82a32ed02afe
Maleesha Indunil <maleeshaindunil2003@gmail.com>
2025-08-27 10:06:00 +0530

    Add PrizePoolSection

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -6,6 +6,7 @@ import { PartnersSection } from "@/components/PartnersSection";
 import { ContactUsSection } from "@/components/ContactUsSection";
 import { RegistrationForm } from "@/components/RegistrationForm";
 import { Footer } from "@/components/Footer";
+import PrizePoolSection from "@/components/PrizePoolSection";
 
 const Index = () => {
   return (
@@ -15,6 +16,7 @@ const Index = () => {
         <HeroSection />
         <div id="about"><AboutSection /></div>
         <div id="timeline"><TimelineSection /></div>
+        <div id="prizes"><PrizePoolSection /></div>
         <div id="partners"><PartnersSection /></div>
         <div id="contact"><ContactUsSection /></div>
         <div id="register"><RegistrationForm /></div>
